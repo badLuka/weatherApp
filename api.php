c54ca8d43caae76998dada8593f4076c9cc7fc8c
mk <mkalugin21@gmail.com>
2023-09-08 22:47:42 +0400

    displayed temperature, cloudiness and wind

--- api.php
+++ api.php
@@ -1,11 +1,17 @@
 <?php
 
+error_reporting(E_ERROR | E_PARSE);
+
+
+
 if (isset($_GET['city'])){
-    $url =  file_get_contents('https://api.openweathermap.org/data/2.5/weather?q='.$_GET['city'].'&appid=343bbc25141e62f27143e8df02d41cd3');
+    $url =  file_get_contents('https://api.openweathermap.org/data/2.5/weather?q='.$_GET['city'].'&appid=343bbc25141e62f27143e8df02d41cd3&units=metric');
 
     $weatherArray = json_decode($url, true);
 
-    print_r($weatherArray);
+    $weatherTemp = round($weatherArray['main']['temp']);
+    $weatherWind = round($weatherArray['wind']['speed']);
+    $weatherCloud = $weatherArray['weather'][0]['description'];
 }
 
 
--- index.php
+++ index.php
@@ -21,32 +21,39 @@ include ('api.php')
 
     <div class="container">
         <div class="content">
-            <h1>Узнай, какая сейчас погода в твоем городе</h1>
+            <h1>Какая сейчас погода в городе</h1>
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Погода в городе</label>
+                    <label for="exampleInputEmail1" class="form-label">Город</label>
                     <input type="text" class="form-control" name="city" id="currentCity" aria-describedby="emailHelp">
-<!--                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Поиск</button>
             </form>
+
+            <?php if ($_GET['city'] == ''): ?>
+
+            <?php else: ?>
+                <div class="weather-info">
+                    <p>Сейчас в <?=$_GET['city']?></p>
+                    <div class="weather-info__temperature">
+                        <p>Температура воздуха <?=$weatherTemp;?>&#8451;</p>
+                    </div>
+                    <div class="weather-info__wind">
+                        <p><?=$weatherWind > 0 ? 'Ветер ' . $weatherWind . ' м/с' : 'Безветренно';?></p>
+                    </div>
+                    <div class="weather-info__cloud">
+                        <p><?=$weatherCloud == 'clear sky' ? 'Ясно ' : 'Облачно';?></p>
+                    </div>
+                </div>
+            <?php endif;?>
+
         </div>
 
     </div>
 
-
-
-
-<!-- Optional JavaScript; choose one of the two! -->
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
+
 </body>
 </html>
 
